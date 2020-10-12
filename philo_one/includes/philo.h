e61829681de9a602006d79c3f8cca7f36d9f21e9
deddara <deddara@student.21-school.ru>
2020-10-12 18:21:36 +0300

    norme

--- philo_one/includes/philo.h
+++ philo_one/includes/philo.h
@@ -16,6 +16,7 @@
 # include <pthread.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/time.h>
 
 typedef struct		s_table
 {
@@ -24,7 +25,6 @@ typedef struct		s_table
 	int				eat_time;
 	int				sleep_time;
 	int				eat_num;
-	int				start;
 	int				smb_died;
 	int				sim_start;
 	pthread_mutex_t	death_mutex;
@@ -36,17 +36,19 @@ typedef struct		s_philo
 	int				id;
 	int				last_lunch_t;
 	int				eat_num;
-	int				cur_t;
-	int				is_died;
 	t_table			*table;
 }					t_philo;
 
 void				ft_putstr_fd(char *s, int fd);
 int					check_for_num(char *str);
 int					ft_atoi(const char *str);
-void				ft_putnbr_fd(int n, int fd);
 char				*ft_itoa(int n);
 char				*ft_strjoin_philo(char *time, char *id, char *action);
 int					ft_strlen(char *str);
+int					init_and_threads(t_table *table, char **argv);
+void				*simulation(void *val);
+void				msg(t_philo *philo, char *action);
+void				my_wait(int time);
+int					take_time_in_ms(void);
 
 #endif
---
+++ philo_one/src/init_and_threads.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_and_threads.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/12 18:19:38 by deddara           #+#    #+#             */
+/*   Updated: 2020/10/12 18:19:39 by deddara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void		init_philo(int i, t_philo *philo, t_table *table)
+{
+	philo->id = i;
+	philo->table = table;
+	philo->eat_num = table->eat_num;
+}
+
+static void		start_threads(t_table *table, pthread_mutex_t *forks)
+{
+	int			i;
+	t_philo		philo[table->phl_num];
+	pthread_t	thread[table->phl_num];
+
+	i = 0;
+	while (i < table->phl_num)
+	{
+		init_philo(i, &philo[i], table);
+		pthread_create(&thread[i], NULL, simulation, &philo[i]);
+		i++;
+	}
+	i = 0;
+	while (i < table->phl_num)
+	{
+		pthread_join(thread[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < table->phl_num)
+	{
+		pthread_mutex_destroy(&forks[i]);
+		i++;
+	}
+}
+
+static void		init_mutex_and_start_threads(t_table *table)
+{
+	int						i;
+	pthread_mutex_t			forks[table->phl_num];
+	static pthread_mutex_t	death_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+	i = 0;
+	while (i < table->phl_num)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+	table->forks = forks;
+	table->death_mutex = death_mutex;
+	start_threads(table, forks);
+	pthread_mutex_destroy(&death_mutex);
+}
+
+int				init_and_threads(t_table *table, char **argv)
+{
+	if (!(table->phl_num = ft_atoi(argv[1])))
+	{
+		ft_putstr_fd("there are no philosophers\n", 2);
+		return (1);
+	}
+	table->die_time = ft_atoi(argv[2]);
+	table->eat_time = ft_atoi(argv[3]);
+	table->sleep_time = ft_atoi(argv[4]);
+	if (!(argv[5]))
+		table->eat_num = -1;
+	else
+		table->eat_num = ft_atoi(argv[5]);
+	table->smb_died = 0;
+	init_mutex_and_start_threads(table);
+	return (0);
+}
